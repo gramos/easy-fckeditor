956cf559dae55c71db19c5235346382582b9f5c6
Gaston Ramos <ramos.gaston@gmail.com>
2008-09-16 16:43:40 -0300

    added fck conf for easy upload plugin

---
+++ public/javascripts/fckcustom_easy.js
@@ -0,0 +1,30 @@
+// CHANGE FOR APPS HOSTED IN SUBDIRECTORY
+FCKRelativePath = '';
+
+// DON'T CHANGE THESE
+FCKConfig.LinkBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Connector='+FCKRelativePath+'/fckeditor/command';
+
+FCKConfig.ImageBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Type=Image&Connector='+FCKRelativePath+'/fckeditor/command';
+
+FCKConfig.FlashBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Type=Flash&Connector='+FCKRelativePath+'/fckeditor/command';
+
+FCKConfig.LinkUploadURL = FCKRelativePath+'/fckeditor/upload';
+FCKConfig.ImageUploadURL = FCKRelativePath + '/fckeditor/upload?Type=Image';
+//FCKConfig.FlashUploadURL = FCKRelativePath + '/fckeditor/upload?Type=Flash';
+//FCKConfig.SpellerPagesServerScript = FCKRelativePath + '/fckeditor/check_spelling';
+FCKConfig.AllowQueryStringDebug = false;
+//FCKConfig.SpellChecker = 'SpellerPages';
+
+FCKConfig.Plugins.Add( 'easyUpload', 'en' ) ;
+
+FCKConfig.ContextMenu = ['Generic','Anchor','Flash','Select','Textarea','Checkbox','Radio','TextField','HiddenField','ImageButton','Button','BulletedList','NumberedList','Table','Form'] ;
+
+// ONLY CHANGE BELOW HERE
+FCKConfig.SkinPath = FCKConfig.BasePath + 'skins/silver/';
+
+FCKConfig.ToolbarSets["Simple"] = [
+        ['Bold','Italic','Underline','StrikeThrough','-'],
+        ['OrderedList','UnorderedList','-'],
+        ['Link','Unlink'], ['Image'], ['FontSize'],
+        ['TextColor','BGColor'], ['easyImage','easyFile','easyLink']
+] ;
